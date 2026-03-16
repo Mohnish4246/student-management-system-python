6809bebd4493c9c0be59be14dbd8c3e3a764fe5b
Mohnish Sharma <mohnishs848@gmail.com>
2026-03-16 10:08:47 +0530

    Initial  commit Student Management System

---
+++ student.py
@@ -0,0 +1,19 @@
+class Student:
+
+    def __init__(self, roll, name, marks):
+        self.roll = roll
+        self.name = name
+        self.marks = marks
+
+
+    def to_dict(self):
+        return {
+            "roll": self.roll,
+            "name": self.name,
+            "marks": self.marks
+        }
+
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["roll"], data["name"], data["marks"]) # converts dictionary → object
